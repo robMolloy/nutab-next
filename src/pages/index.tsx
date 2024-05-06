5aad34e3b6e09406c858cb50dbc25eb0f7181901
robMolloy <robmolloy@hotmail.co.uk>
2024-05-06 11:47:14 +0100

    theme setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
+import { Button } from "@/components";
 import { Flash, useFlash } from "@/modules";
+import { useThemeStore } from "@/stores";
+import React from "react";
 
 const Parent = () => {
   const { flash, flashSignal } = useFlash();
+  const themeStore = useThemeStore();
 
   return (
     <main className={`min-h-screen`}>
+      <Button variant="neutral" onClick={() => themeStore.setRandomThemeName()}>
+        toggleTheme
+      </Button>
+      {themeStore.themeName}
+      <br />
       <button onClick={flash}>add flash asdxfrom parent</button>
       <div style={{ height: "37vh", width: `${(37 * 6) / 4}vh` }}>
         <Flash flashSignal={flashSignal} />
---
+++ src/stores/useThemeStore.ts
@@ -0,0 +1,63 @@
+import { create } from "zustand";
+import { Theme as TThemeName } from "daisyui";
+
+const themeNames = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee",
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi",
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk",
+  "autumn",
+  "business",
+  "acid",
+  "lemonade",
+  "night",
+  "coffee",
+  "winter",
+  "dim",
+  "nord",
+  "sunset",
+] as const;
+
+interface ThemeState {
+  themeNames: typeof themeNames;
+  themeName: TThemeName;
+  setThemeName: (themeName: TThemeName) => void;
+  setRandomThemeName: () => void;
+}
+
+export const useThemeStore = create<ThemeState>()((set, get) => ({
+  themeNames,
+  themeName: "cupcake",
+  setThemeName: (themeName: TThemeName) => {
+    const htmlElm = document.querySelector("html");
+    if (!htmlElm)
+      throw new Error("can't find html element so unable to change theme");
+
+    htmlElm.setAttribute("data-theme", themeName);
+    set(() => ({ themeName: themeName }));
+  },
+  setRandomThemeName: () => {
+    const randomNumber = Math.floor(Math.random() * themeNames.length);
+    const newThemeName = get().themeNames[randomNumber];
+
+    if (newThemeName) get().setThemeName(newThemeName);
+  },
+}));
