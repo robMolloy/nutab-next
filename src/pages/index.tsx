6ddce71b9c5389f6fe626f640ce605f8eec3e361
robMolloy <robmolloy@hotmail.co.uk>
2024-05-13 01:09:59 +0100

    flow working

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components";
 import { Flash } from "@/modules";
 import { DumbVideoStream } from "@/modules/videoStream";
 import {
@@ -7,15 +8,19 @@ import {
 import { useSignal } from "@/utils/useSignal";
 import { useState } from "react";
 
-// @State() status:
-//     | 'loading'
-//     | 'preReady'
-//     | 'ready'
-//     | 'capturing'
-//     | 'selecting'
-//     | 'sending'
-//     | 'fail'
-//     | 'success' = 'loading';
+const delay = async (x: number) => {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(true), x);
+  });
+};
+
+type TFlowStatus =
+  | "capturing"
+  | "ready"
+  | "selecting"
+  | "sending"
+  | "fail"
+  | "success";
 
 const useFlow = () => {
   const [imageDataUrl, setImageDataUrl] = useState<string | undefined>();
@@ -28,13 +33,11 @@ const useFlow = () => {
     captureSignal.changeSignal();
   };
 
-  const status = (() => {
-    if (imageDataUrl) return "selecting";
-    return "capturing";
-  })();
+  const [status, setStatus] = useState<TFlowStatus>("capturing");
 
   return {
     status,
+    setStatus,
     imageDataUrl,
     setImageDataUrl,
     flashSignal,
@@ -46,6 +49,7 @@ const useFlow = () => {
 const Parent = () => {
   const {
     status,
+    setStatus,
     imageDataUrl,
     setImageDataUrl,
     flashSignal,
@@ -54,10 +58,25 @@ const Parent = () => {
   } = useFlow();
 
   return (
-    <main className={`min-h-screen`} onClick={() => capture()}>
-      <pre>{JSON.stringify({ status, imageDataUrl }, undefined, 2)}</pre>
-
-      <h2>Tap anywhere to take photo</h2>
+    <main
+      className={`min-h-screen`}
+      onClick={() => {
+        if (status !== "capturing") return;
+        capture();
+        setStatus("selecting");
+      }}
+    >
+      <br />
+      <div className="prose w-full m-auto">
+        <h2 className="text-center">
+          {status === "capturing" && "Tap anywhere to take photo"}
+          {status === "selecting" && "Select photo or discard"}
+          {status === "sending" && "Please wait..."}
+          {status === "fail" && "Something has gone wrong, please try again"}
+          {status === "success" && "Success! Please return the device"}
+        </h2>
+      </div>
+      <br />
 
       <div className="flex justify-center items-center">
         <VideoStreamContainer>
@@ -66,15 +85,16 @@ const Parent = () => {
               <Flash signal={flashSignal} />
             </VideoStreamContainerItem>
           )}
-          {status === "capturing" && (
+          {(status === "capturing" || status === "selecting") && (
             <VideoStreamContainerItem>
               <DumbVideoStream
                 signal={captureSignal}
                 onCapture={(x) => setImageDataUrl(x)}
+                className={`${status === "selecting" ? "invisible" : ""}`}
               />
             </VideoStreamContainerItem>
           )}
-          {status === "selecting" && (
+          {(status === "selecting" || status === "sending") && (
             <VideoStreamContainerItem>
               <div
                 className={`flex w-full h-full`}
@@ -87,8 +107,71 @@ const Parent = () => {
               />
             </VideoStreamContainerItem>
           )}
+          {status === "sending" && (
+            <VideoStreamContainerItem>
+              <div
+                className={`flex w-full h-full justify-center items-center bg-gray-400 opacity-60`}
+              >
+                <span className="loading loading-spinner loading-lg" />
+              </div>
+            </VideoStreamContainerItem>
+          )}
+          {status === "fail" && (
+            <VideoStreamContainerItem>
+              <div className="flex justify-center w-full h-full items-center">
+                <div className="flex flex-col items-center gap-2">
+                  <Button
+                    variant="error"
+                    onClick={async () => setStatus("selecting")}
+                  >
+                    Try again
+                  </Button>
+                </div>
+              </div>
+            </VideoStreamContainerItem>
+          )}
+          {status === "success" && (
+            <VideoStreamContainerItem>
+              <div className="flex justify-center w-full h-full items-center">
+                <div className="flex flex-col items-center gap-2">
+                  <Button
+                    variant="ghost"
+                    outline
+                    onClick={async () => setStatus("capturing")}
+                  >
+                    Start again
+                  </Button>
+                </div>
+              </div>
+            </VideoStreamContainerItem>
+          )}
         </VideoStreamContainer>
       </div>
+      <br />
+      {status === "selecting" && (
+        <div className="flex justify-center gap-2">
+          <Button
+            variant="primary"
+            onClick={async () => {
+              setStatus("sending");
+              await delay(1000);
+              setStatus(Math.random() > 0.3 ? "success" : "fail");
+            }}
+          >
+            Select
+          </Button>
+          <Button
+            variant="ghost"
+            outline
+            onClick={() => {
+              setImageDataUrl(undefined);
+              setStatus("capturing");
+            }}
+          >
+            Discard
+          </Button>
+        </div>
+      )}
     </main>
   );
 };
